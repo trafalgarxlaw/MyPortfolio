8c8f4fba702acdb2ff5d1a35ab0ed4e60d20492c
Trafalgar <masyyaf@gmail.com>
2019-08-02 03:37:42 -0400

    commit

--- myScript.js
+++ myScript.js
@@ -1,4 +1,25 @@
-function myCfunction(){
+let opened=false;
+let clicked=false;
+let Chekboxopened=false;
+
+function myCfunction(){ 
+    if(window.innerWidth < 864){    
+    if(opened){
+    document.getElementById('contact').style.fontSize = "14px";
+    document.getElementById('contact').innerHTML = "contact";
+    }
+    else{
     document.getElementById('contact').style.fontSize = "30px";
-    document.getElementById('contact').innerText = "TA RACE CHAKIB";
+    document.getElementById('contact').style.transition = "all 1s";
+    document.getElementById('contact').innerHTML = "masyyaf@gmail.com";
+    document.getElementById('contact').href="masyyaf@gmail.com";
+    }
+    opened=!opened;
+}
+}
+function Chekbox(){
+    if(Chekboxopened==false){
+        document.getElementById('contact').style.fontSize = "14px";
+        document.getElementById('contact').innerHTML = "contact";
+    }
 }
